9c357d3882687b294f60f6108e1afcf3da837313
aviiciii <laavesh1@gmail.com>
2022-11-13 23:18:42 +0530

    created tables for inbox

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -27,8 +27,8 @@ function compose_email() {
 		const recipients = document.querySelector('#compose-recipients').value;
 		const subject = document.querySelector('#compose-subject').value;
 		const body = document.querySelector('#compose-body').value;
+		
 		// Send email
-		console.log(recipients);
 		fetch('/emails', {
 			method: 'POST',
 			body: JSON.stringify({
@@ -57,4 +57,48 @@ function load_mailbox(mailbox) {
 
 	// Show the mailbox name
 	document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+	fetch('/emails/inbox')
+	.then(response => response.json())
+	.then(emails => {
+		// Print emails
+		console.log(emails);
+
+		// ... do something else with emails ...
+		// emails not empty
+		if (emails.length > 0) {
+			const list = document.createElement('table');
+			list.className= "table table-dark table-striped table-hover";
+			list.id = "emails-table"
+			document.querySelector('#emails-view').append(list)
+			// iterate over emails
+			for (let index = 0; index < emails.length; index++) {
+				const mail = emails[index];
+				// create row
+				const row = document.createElement('tr');
+				row.className = "table-dark";
+				row.id='t'+index;
+				document.querySelector('#emails-table').append(row)
+
+				// create cells
+				const sender = document.createElement('td');
+				const subject = document.createElement('td');
+				const time = document.createElement('td');
+
+				sender.className= subject.className = time.className = "table-dark";
+
+				sender.innerHTML = mail.sender;
+				subject.innerHTML = mail.subject;
+				time.innerHTML = mail.timestamp;
+
+				// append cells
+				document.querySelector('#t'+index).append(sender)
+				document.querySelector('#t'+index).append(subject)
+				document.querySelector('#t'+index).append(time)
+
+			}
+			
+		}
+	});
+
 }
